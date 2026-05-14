3441699255b9309f096c8f5b6a12930481e0b98a
Xyc| Artem K. <zoom.2000v@gmail.com>
2026-05-14 19:09:06 +0300

    add auth controller

---
+++ backend/controllers/authControllers.js
@@ -0,0 +1,19 @@
+const authService = require('../services/authService');
+
+exports.signup = async (req, res, next) => {
+  try {
+    const token = await authService.signup(req.body);
+    res.status(201).json({ token });
+  } catch (e) {
+    next(e);
+  }
+};
+
+exports.signin = async (req, res, next) => {
+  try {
+    const token = await authService.signin(req.body);
+    res.status(200).json({ token });
+  } catch (e) {
+    next(e);
+  }
+};
