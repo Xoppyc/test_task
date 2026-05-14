694244c93c30944dd5b22edcea8fdd85838fb81a
Xyc| Artem K. <zoom.2000v@gmail.com>
2026-05-15 00:10:05 +0300

    update auth controller to return both token and user data

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,9 +1,10 @@
 const authService = require('../services/authService');
 
+// I really don't know if it's a good practice,
 exports.signup = async (req, res, next) => {
   try {
-    const token = await authService.signup(req.body);
-    res.status(201).json({ token });
+    const { token, user } = await authService.signup(req.body);
+    res.status(201).json({ token, user });
   } catch (e) {
     next(e);
   }
@@ -11,8 +12,8 @@ exports.signup = async (req, res, next) => {
 
 exports.login = async (req, res, next) => {
   try {
-    const token = await authService.login(req.body);
-    res.status(200).json({ token });
+    const { token, user } = await authService.login(req.body);
+    res.status(200).json({ token, user });
   } catch (e) {
     next(e);
   }
