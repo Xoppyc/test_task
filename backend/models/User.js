dd435cd8be2a29d7e38afe4a1554c9df5eadd661
Xyc| Artem K. <zoom.2000v@gmail.com>
2026-05-14 17:34:12 +0300

    add User model

---
+++ backend/models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['user', 'admin'], required: true },
+});
+
+userSchema.pre('save', async function () {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
