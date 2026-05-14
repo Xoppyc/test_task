4877d782a11f6f75cda7fb7964975a412cee323c
Xyc| Artem K. <zoom.2000v@gmail.com>
2026-05-15 01:10:30 +0300

    fixed isue with missing/wrong imports

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './main.css'
+import './main.scss'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router'
 import { AuthProvider } from './context/authContext.tsx'
